3af9521cc7e9e76ead5351a54c1c4369b4d40857
Michail <mishamal31082008@gmail.com>
2026-02-18 23:17:37 +0500

    Update AI_bot.py

--- AI_bot.py
+++ AI_bot.py
@@ -1,30 +1,30 @@
 from openai import OpenAI
 import telebot
 
-token = '8146333746:AAFw-Jj-BghNwORJ5PUQg8bPFEwMtcDoTGg'
+token = 'YOUR_BOT_TOKEN'
 bot = telebot.TeleBot(token)
 
 client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key="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",
+    base_url="BASE_URL_FROM_OPENROUTER",
+    api_key="API_KEY_OF_CLIENT",
 )
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Привет! \n"
-                          "Я твой искусственный ассистент. Что сегодня ты хочешь узнать? \n"
-                          "Чтобы задать вопрос следуй инструкции: /tell <вопрос>")
+    bot.reply_to(message, "Hi! \n"
+                          "I'm your artificial assistant. What do you want to know today? \n"
+                          "To ask a question, follow the instructions: /tell <question>")
 
 @bot.message_handler(commands=['tell'])
 def tell(message):
     question = message.text.split(' ', 1)[1]
     completion = client.chat.completions.create(
         extra_headers={
-            "HTTP-Referer": "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", # Optional. Site URL for rankings on openrouter.ai.
-            "X-Title": "Tg_Bot",  # Optional. Site title for rankings on openrouter.ai.
+            "HTTP-Referer": "API_KEY_OF_CLIENT",
+            "X-Title": "Tg_Bot",
         },
         extra_body={},
-        model="nousresearch/deephermes-3-mistral-24b-preview:free",
+        model="API_OF_MODEL",
         messages=[
           {
             "role": "user",
@@ -35,4 +35,5 @@ def tell(message):
     bot.reply_to(message, completion.choices[0].message.content)
 
 bot.polling(none_stop=True)
+
 
